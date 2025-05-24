be844816705440cac82ef6ae0b8b5081037c8b38
Tomi <tomazslo.eu@gmail.com>
2025-05-24 15:05:56 +0200

    Fix sys.path and imports to respect dynamic_scenes package structure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
-# tests/conftest.py
 import sys
 import os
 
-base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../custom_components/dynamic_scenes'))
-sys.path.insert(0, base_dir)
+# Dodamo pot do custom_components/
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../custom_components')))
